e97b7e08815627da9030a8b837a841d69d868eb5
Obernatskiy <sasha236541@gmail.com>
2022-06-24 14:13:56 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import './App.css';
 
-import Character from "./component/character";
-
 function App() {
     return (
         <div>
-            <Character
-                name={'bart'}
-                desc={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, odit!"}
-                picture={"https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png"}/>
-
-            <Character
-            name={"Homer"}
-            desc={"Lorem ipsum dolor sit amet."}
-            picture={"https://www.onthisday.com/images/people/homer-simpson-medium.jpg"}/>
         </div>
     );
 }
