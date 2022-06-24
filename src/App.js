6efa9cef6a520c21fbd44bfe0e353efca021f5b2
Obernatskiy <sasha236541@gmail.com>
2022-06-24 12:52:41 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Character from "./component/character";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Character
+                name={'bart'}
+                desc={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, odit!"}
+                picture={"https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png"}/>
+
+            <Character
+            name={"Homer"}
+            desc={"Lorem ipsum dolor sit amet."}
+            picture={"https://www.onthisday.com/images/people/homer-simpson-medium.jpg"}/>
+        </div>
+    );
 }
 
 export default App;
